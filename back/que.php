38fc84019133c453813527cd128adafbce47e981
wei <lin951016@gmail.com>
2024-01-05 14:38:44 +0800

    新增問卷題目

--- back/que.php
+++ back/que.php
@@ -1,30 +1,29 @@
 <fieldset>
     <legend>新增問卷</legend>
     <form action="./api/add_que.php" method="post">
-        <div style="display:flex;">
-            <div>問券名稱</div>
-            <div>
-                <input type="text" name="subject" id="">
-            </div>
-        </div>
+    <div style="display: flex;">
+        <div>問卷名稱</div>
         <div>
-            <div id="opt">選項
-                <input type="text" name="option" id="">
-                <input type="button" value="更多" onclick="more()">
-            </div>
+            <input type="text" name="subject">
+        </div>
+    </div>
+    <div>
+        <div id="opt">選項
+            <input type="text" name="option[]">
+            <input type="button" value="更多" onclick="more()">
         </div>
 
-        <div class="ct">
-            <input type="submit" value="送出"><input type="reset" value="清除">
-        </div>
-    </form>
-
+    </div>
+<div class="ct">
+    <input type="submit" value="送出"><input type="reset" value="清空">
+</div>
+</form>
 </fieldset>
 <script>
-    function more(){
-        let opt = `<div id="opt">選項
-                <input type="text" name="option" id="">              
+function more(){
+    let opt=`<div id="opt">選項
+                <input type="text" name="option[]">
             </div>`
-        $("#opt").before(opt);
-    }
+    $("#opt").before(opt);
+}    
 </script>
