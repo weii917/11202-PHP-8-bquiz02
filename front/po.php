b7d95ffc209ba25298ab7db6021da8b44d43c190
wei <lin951016@gmail.com>
2024-01-05 15:58:48 +0800

    使用ajax來載入分類網誌的標題

---
+++ front/po.php
@@ -0,0 +1,45 @@
+<style>
+    .type-item{
+        display: block;
+        margin:3px 6px;
+    }
+    .types,.news-list{
+        display: inline-block;
+        vertical-align: top;
+    }
+    .news-list{
+        width:600px;
+    }
+</style>
+<div class="nav">目前位置:首頁 > 分類網誌 > <span class="type">健康新知</span></div>
+
+<fieldset class='types'>
+    <legend>分類網誌</legend>
+<a class='type-item' data-id="1">健康新知</a>
+<a class='type-item' data-id="2">菸害防治</a>
+<a class='type-item' data-id="3">癌症防治</a>
+<a class='type-item' data-id="4">慢性病防治</a>
+</fieldset>
+<fieldset class='news-list'>
+    <legend>文章列表</legend>
+    <div class="list-items">
+        
+    </div>
+    <div class="article"></div>
+</fieldset>
+
+<script>
+getList(1)    
+    $(".type-item").on('click',function(){
+        $(".type").text($(this).text())
+        let type=$(this).data('id')
+        getList(type)
+
+    })
+function getList(type){
+    $.get("./api/get_list.php",{type},(list)=>{
+        $(".list-items").html(list)
+    })
+}
+</script>
+
