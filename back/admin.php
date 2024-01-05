efdb40df804bb6f1102f16ba1bc2bd3d038af280
wei <lin951016@gmail.com>
2024-01-05 11:12:57 +0800

    做帳號編輯功能，不顯示管理帳號防止可選擇刪除

--- back/admin.php
+++ back/admin.php
@@ -1,25 +1,29 @@
 <fieldset>
     <legend>帳號管理</legend>
-    <form action="./edit_user.php" method="post">
+    <form action="./api/edit_user.php" method="post">
         <table style="width:55%;margin:auto;text-align:center">
             <tr>
-                <td class="col">帳號</td>
-                <td class="col">密碼</td>
-                <td class="col">刪除</td>
+                <td class="clo">帳號</td>
+                <td class="clo">密碼</td>
+                <td class="clo">刪除</td>
             </tr>
             <?php
-            $rows=$User->all();
-            foreach($rows as $row){           
+            $rows = $User->all();
+            foreach ($rows as $row) {
+                if ($row['acc'] != 'admin') {
+
+
             ?>
-            <tr>
-                <td><?=$row['acc']?></td>
-                <td><?=str_repeat("*",mb_strlen($row['pw']));?></td>
-                <td>
-                    <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
-                </td>
-            </tr>
+                    <tr>
+                        <td><?= $row['acc'] ?></td>
+                        <td><?= str_repeat("*", mb_strlen($row['pw'])); ?></td>
+                        <td>
+                            <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
+                        </td>
+                    </tr>
             <?php
-             }
+                }
+            }
             ?>
         </table>
         <div class="ct">
