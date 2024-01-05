d122639dd4a4388ba3fab5ab532c32ac748f9f46
wei <lin951016@gmail.com>
2024-01-05 09:41:49 +0800

    完成主題內容點選切換

--- front/main.php
+++ front/main.php
@@ -17,6 +17,7 @@
         border: 1px solid black;
         min-height: 400px;
         margin-top: -1px;
+        display: none;
     }
     .active{
         border-bottom:1px solid white;
@@ -24,23 +25,26 @@
 </style>
 
 <div class="tags">
-    <div class="tag active">健康新知</div>
-    <div class="tag">菸害防治</div>
-    <div class="tag">癌症防治</div>
-    <div class="tag">慢性病防治</div>
+    <div id="sec01" class="tag active">健康新知</div>
+    <div id="sec02" class="tag">菸害防治</div>
+    <div id="sec03" class="tag">癌症防治</div>
+    <div id="sec04" class="tag">慢性病防治</div>
 </div>
 
 <article>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
+    <section id="section01" style="display:block"><h2>健康新知</h2></section>
+    <section id="section02"><h2>菸害防治</h2></section>
+    <section id="section03"><h2>癌症防治</h2></section>
+    <section id="section04"><h2>慢性病防治</h2></section>
 </article>
 
 <script>
    $(".tag").on('click',function(){
         $(".tag").removeClass('active')
         $(this).addClass('active')
+        let id=$(this).attr('id').replace("sec",'section');
+        $("section").hide();
+        $("#"+id).show();
    })
 </script>
 
