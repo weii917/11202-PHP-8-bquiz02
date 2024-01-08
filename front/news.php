803906a8dedae10965f5aac88008438ceb8a7ad1
wei <lin951016@gmail.com>
2024-01-08 09:25:47 +0800

    顯示最新文章區與人氣文章標題內容

---
+++ front/news.php
@@ -0,0 +1,24 @@
+<fieldset>
+    <legend>目前位置:首頁 > 最新文章區</legend>
+    <table style="width:95%;margin:auto">
+        <tr>
+            <th width="30%">標題</th>
+            <th width="50%">內容</th>
+            <th></th>
+        </tr>
+
+        <?php
+        $rows = $News->all(['sh' => 1]);
+        foreach ($rows as $row) {
+        ?>
+            <tr>
+                <td><?=$row['title'];?></td>
+                <td><?=mb_substr($row['news'],0,25);?>...</td>
+                <td></td>
+            </tr>
+
+        <?php
+        }
+        ?>
+    </table>
+</fieldset>
---
+++ front/pop.php
@@ -0,0 +1,24 @@
+<fieldset>
+    <legend>目前位置:首頁 > 人氣文章區</legend>
+    <table style="width:95%;margin:auto">
+        <tr>
+            <th width="30%">標題</th>
+            <th width="50%">內容</th>
+            <th>人氣</th>
+        </tr>
+
+        <?php
+        $rows = $News->all(['sh' => 1], "order by `good` desc");
+        foreach ($rows as $row) {
+        ?>
+            <tr>
+                <td><?=$row['title'];?></td>
+                <td><?=mb_substr($row['news'],0,25);?>...</td>
+                <td></td>
+            </tr>
+
+        <?php
+        }
+        ?>
+    </table>
+</fieldset>
