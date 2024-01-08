0b494f68fdd2f54ec6aca33fba6d4333b613c5c8
wei <lin951016@gmail.com>
2024-01-08 09:59:53 +0800

    完成文章區分頁功能

--- front/news.php
+++ front/news.php
@@ -8,12 +8,17 @@
         </tr>
 
         <?php
-        $rows = $News->all(['sh' => 1]);
+        $total = $News->count(['sh' => 1]);
+        $div = 5;
+        $pages = ceil($total / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * 5;
+        $rows = $News->all(['sh' => 1], " limit $start,$div");
         foreach ($rows as $row) {
         ?>
             <tr>
-                <td><?=$row['title'];?></td>
-                <td><?=mb_substr($row['news'],0,25);?>...</td>
+                <td><?= $row['title']; ?></td>
+                <td><?= mb_substr($row['news'], 0, 25); ?>...</td>
                 <td></td>
             </tr>
 
@@ -21,4 +26,25 @@
         }
         ?>
     </table>
+    <div>
+        <?php
+        if ($now - 1 > 0) {
+            $prev = $now - 1;
+            echo "<a href='?do=news&p=$prev'> ";
+            echo " < ";
+            echo " </a>";
+        }
+        for ($i = 1; $i <= $pages; $i++) {
+            $fontsize = ($i == $now) ? "font-size:22px;" : "font-size:16px;";
+            echo "<a href='?do=news&p=$i' style='$fontsize'> $i </a>";
+        }
+        if ($now + 1 <= $pages) {
+            $next = $now + 1;
+            echo "<a href='?do=news&p=$next'> ";
+            echo " > ";
+            echo " </a>";
+        }
+        ?>
+    </div>
+
 </fieldset>
--- front/pop.php
+++ front/pop.php
@@ -8,12 +8,17 @@
         </tr>
 
         <?php
-        $rows = $News->all(['sh' => 1], "order by `good` desc");
+        $total = $News->count(['sh' => 1]);
+        $div = 5;
+        $pages = ceil($total / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * 5;
+        $rows = $News->all(['sh' => 1], "order by `good` desc limit $start,$div");
         foreach ($rows as $row) {
         ?>
             <tr>
-                <td><?=$row['title'];?></td>
-                <td><?=mb_substr($row['news'],0,25);?>...</td>
+                <td><?= $row['title']; ?></td>
+                <td><?= mb_substr($row['news'], 0, 25); ?>...</td>
                 <td></td>
             </tr>
 
@@ -21,4 +26,24 @@
         }
         ?>
     </table>
+    <div>
+        <?php
+        if ($now - 1 > 0) {
+            $prev = $now - 1;
+            echo "<a href='?do=pop&p=$prev'> ";
+            echo " < ";
+            echo " </a>";
+        }
+        for ($i = 1; $i <= $pages; $i++) {
+            $fontsize = ($i == $now) ? "font-size:22px;" : "font-size:16px;";
+            echo "<a href='?do=pop&p=$i' style='$fontsize'> $i </a>";
+        }
+        if ($now + 1 <= $pages) {
+            $next = $now + 1;
+            echo "<a href='?do=pop&p=$next'> ";
+            echo " > ";
+            echo " </a>";
+        }
+        ?>
+    </div>
 </fieldset>
